7a9383b636c3eb92c6a2ac64e125b5939faa7353
MasakiFukunishi <fmsknn@gmail.com>
2023-08-30 12:10:21 -0700

    Add api

--- server/controllers/candles.js
+++ server/controllers/candles.js
@@ -1,6 +1,6 @@
 import { Candle } from "../models/candle.js";
 
-export const getAllCnadles = async function (req, res) {
+export const getAllCandles = async function (req, res) {
   const candles = await Candle.find();
   res.json(candles);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
+import candleApi from "../api/candle";
+
 const Home = () => {
+  useEffect(() => {
+    console.log("Home");
+    candleApi.getAll().then((_candles) => {
+      console.log(_candles);
+    });
+  }, []);
   return (
     <div>
       <h1>Home</h1>
