030e14d99188d3784085911c743b0d6954608b3a
MasakiFukunishi <fmsknn@gmail.com>
2023-08-31 15:48:38 -0700

    Add chart components

---
+++ src/components/charts/CandlestickChart.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef } from "react";
+import ApexCharts from "apexcharts";
+
+interface CandlestickChartProps {
+  data: any;
+}
+
+const CandlestickChart: React.FC<CandlestickChartProps> = ({ data }) => {
+  const chartRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      series: [
+        {
+          data: data,
+        },
+      ],
+      plotOptions: {
+        candlestick: {
+          colors: {
+            upward: "#54f542",
+            downward: "#EF403C",
+          },
+          wick: {
+            useFillColor: true,
+          },
+        },
+      },
+      chart: {
+        id: "candles",
+        type: "candlestick",
+        height: 400,
+        background: "#1E1E1E",
+        foreColor: "#fff",
+        toolbar: {
+          show: false,
+        },
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        tooltip: {
+          enabled: true,
+        },
+        labels: {
+          minWidth: 40,
+          maxWidth: 40,
+          formatter: (val: number) => {
+            return val.toFixed(0);
+          },
+        },
+      },
+    };
+    const chart = new ApexCharts(chartRef.current, options);
+    chart.render();
+
+    return () => {
+      chart.destroy();
+    };
+  }, [data]);
+  return <div ref={chartRef}></div>;
+};
+export default CandlestickChart;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import ApexCharts from "apexcharts";
 
 import ohlcvApi from "../api/ohlcv";
+import CandlestickChart from "../components/charts/CandlestickChart";
+import VolumeBarChart from "../components/charts/VolumeBarChart";
 
 const Home = () => {
   const [ohlcv, setOhlcv] = useState({ ohlc: [], volume: [] });
@@ -17,129 +18,11 @@ const Home = () => {
     fetchOhlcvData();
   }, []);
 
-  useEffect(() => {
-    if (ohlcv.ohlc.length === 0) return;
-
-    const options = {
-      series: [
-        {
-          data: ohlcv.ohlc,
-        },
-      ],
-      plotOptions: {
-        candlestick: {
-          colors: {
-            upward: "#54f542",
-            downward: "#EF403C",
-          },
-          wick: {
-            useFillColor: true,
-          },
-        },
-      },
-      chart: {
-        id: "candles",
-        type: "candlestick",
-        height: 450,
-        background: "#1E1E1E",
-        foreColor: "#fff",
-        toolbar: {
-          show: false,
-        },
-      },
-      xaxis: {
-        type: "datetime",
-      },
-      yaxis: {
-        tooltip: {
-          enabled: true,
-        },
-        labels: {
-          minWidth: 40,
-          maxWidth: 40,
-          formatter: (val: number) => {
-            return val.toFixed(0);
-          },
-        },
-      },
-    };
-
-    const chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
-
-    const optionsBar = {
-      series: [
-        {
-          name: "volume",
-          data: ohlcv.volume,
-        },
-      ],
-      chart: {
-        height: 160,
-        type: "bar",
-        brush: {
-          enabled: false,
-          target: "candles",
-        },
-        background: "#1E1E1E",
-        foreColor: "#fff",
-        toolbar: {
-          show: false,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      plotOptions: {
-        bar: {
-          columnWidth: "80%",
-          colors: {
-            ranges: [
-              {
-                from: -1000,
-                to: 0,
-                color: "#F15B46",
-              },
-              {
-                from: 1,
-                to: 10000,
-                color: "#FEB019",
-              },
-            ],
-          },
-        },
-      },
-      stroke: {
-        width: 0,
-      },
-      xaxis: {
-        type: "datetime",
-      },
-      yaxis: {
-        labels: {
-          minWidth: 40,
-          maxWidth: 40,
-          formatter: (val: number) => {
-            return val.toFixed(0);
-          },
-        },
-      },
-    };
-    console.log(ohlcv.volume);
-    const chartBar = new ApexCharts(document.querySelector("#chart-bar"), optionsBar);
-    chartBar.render();
-
-    return () => {
-      chart.destroy();
-      chartBar.destroy();
-    };
-  }, [ohlcv]);
-
   return (
     <div>
       <h1>Home</h1>
-      <div id="chart"></div>
-      <div id="chart-bar"></div>
+      <CandlestickChart data={ohlcv.ohlc} />
+      <VolumeBarChart data={ohlcv.volume} />
     </div>
   );
 };
