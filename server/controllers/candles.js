7c7b355c74e9783447508443d54449529ab3c873
MasakiFukunishi <fmsknn@gmail.com>
2023-08-30 23:15:06 -0700

    Add chart

--- server/controllers/candles.js
+++ server/controllers/candles.js
@@ -3,9 +3,13 @@ import Candle from "../models/candle.js";
 export const getAllCandles = async function (req, res) {
   const collectionName = "candles_btc_usd";
   const CandleModel = Candle(collectionName);
-  const candles = await CandleModel.find().catch((err) => {
-    console.log(err);
+  const candles = await CandleModel.find();
+  // convert to chart data format
+  const formattedCandles = candles.map((candle) => {
+    return {
+      x: new Date(candle.time),
+      y: [candle.open, candle.high, candle.low, candle.close],
+    };
   });
-  console.log(candles);
-  res.json(candles);
+  res.json(formattedCandles);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,59 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import ApexCharts from "apexcharts";
 
 import candleApi from "../api/candle";
 
 const Home = () => {
+  const [candleData, setCandleData] = useState([]);
   useEffect(() => {
     console.log("Home");
     candleApi.getAll().then((_candles) => {
+      setCandleData(_candles);
       console.log(_candles);
     });
+
+    const options = {
+      series: [
+        {
+          data: candleData,
+        },
+      ],
+      plotOptions: {
+        candlestick: {
+          colors: {
+            upward: "#000",
+            downward: "#EF403C",
+          },
+          wick: {
+            useFillColor: true,
+          },
+        },
+      },
+      chart: {
+        type: "candlestick",
+        height: 350,
+      },
+      title: {
+        text: "CandleStick Chart",
+        align: "left",
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        tooltip: {
+          enabled: true,
+        },
+      },
+    };
+
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
   }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1 id="chart">Home</h1>
     </div>
   );
 };
