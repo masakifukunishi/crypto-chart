e78c048d5996c9de8b2c86196421937a54b4bd40
MasakiFukunishi <fmsknn@gmail.com>
2023-09-01 10:23:32 -0700

    add custom hook

--- src/hook/useFetchOhlcvData.tsx
+++ src/hook/useFetchOhlcvData.tsx
@@ -1,19 +1,27 @@
-import { useEffect } from "react";
-
+import { useState, useEffect } from "react";
 import ohlcvApi from "../api/ohlcv";
 
-const useFetchOhlcvData = (chartPeriod, setOhlcv) => {
+interface OhlcvData {
+  ohlc: any[];
+  volume: any[];
+}
+
+const useFrtchOhlcvData = (chartPeriod: string, currencyPair: string): OhlcvData => {
+  const [ohlcv, setOhlcv] = useState({ ohlc: [], volume: [] });
+
   useEffect(() => {
     const fetchOhlcvData = async () => {
       try {
         const _ohlcv = await ohlcvApi.get(chartPeriod);
         setOhlcv(_ohlcv);
       } catch (error) {
-        console.error("Error fetching candle data:", error);
+        console.error("Error fetching ohlcv data:", error);
       }
     };
     fetchOhlcvData();
-  }, [chartPeriod, ohlcvApi, setOhlcv]);
+  }, [chartPeriod, currencyPair]);
+
+  return ohlcv;
 };
 
-export default useFetchOhlcvData;
+export default useFrtchOhlcvData;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,18 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
-import ohlcvApi from "../api/ohlcv";
-import constantsApi from "../api/constants";
 import Header from "../components/header/Index";
 import CandlestickChart from "../components/charts/CandlestickChart";
 import VolumeBarChart from "../components/charts/VolumeBarChart";
 import { selectChartPeriod, selectCurrencyPair } from "../store/slicers/chart";
-import { setChartConstant, initializeChartConstant } from "../store/slicers/constant";
+import useFetchOhlcvData from "../hook/useFetchOhlcvData";
+import useFetchConstants from "../hook/useFetchConstants";
 
 const Home: React.FC = () => {
-  const dispatch = useDispatch();
   const chartPeriod = useSelector(selectChartPeriod);
   const currencyPair = useSelector(selectCurrencyPair);
 
-  const [ohlcv, setOhlcv] = useState({ ohlc: [], volume: [] });
-
-  useEffect(() => {
-    const fetchOhlcvData = async () => {
-      try {
-        const _ohlcv = await ohlcvApi.get(chartPeriod);
-        setOhlcv(_ohlcv);
-      } catch (error) {
-        console.error("Error fetching candle data:", error);
-      }
-    };
-    fetchOhlcvData();
-  }, [currencyPair, chartPeriod]);
-
-  useEffect(() => {
-    const fetchConstants = async () => {
-      try {
-        const _chartConstants = await constantsApi.geChart();
-        dispatch(setChartConstant(_chartConstants));
-      } catch (error) {
-        console.error("Error fetching constants:", error);
-      }
-    };
-    fetchConstants();
-    return () => {
-      dispatch(initializeChartConstant());
-    };
-  }, []);
+  const ohlcv = useFetchOhlcvData(chartPeriod, currencyPair);
+  useFetchConstants("chart");
 
   return (
     <div className="bg-gray-900 text-gray-50">
