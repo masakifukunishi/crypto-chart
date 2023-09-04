364db7c1c4d20c1cbd9a260962af1dfd8c08ac2f
MasakiFukunishi <fmsknn@gmail.com>
2023-09-04 15:14:16 -0700

    create daily barch

--- server/batch/daily.ts
+++ server/batch/daily.ts
@@ -1,16 +1,25 @@
 import config from "config";
+import cron from "node-cron";
 
 import { CryptowatchConfig } from "../../config/config.js";
 import CryptowatchOhlcv from "./lib/cryptowatch/ohlcv.js";
 
-const cryptowatchConfig: CryptowatchConfig = config.get("cryptowatch");
-const quoteAssets = cryptowatchConfig.quoteAssets;
-const baseAsset = cryptowatchConfig.baseAsset;
-const exchange = cryptowatchConfig.exchange;
-const initDataNum = cryptowatchConfig.initDataNum;
+async function processData() {
+  const cryptowatchConfig: CryptowatchConfig = config.get("cryptowatch");
+  const quoteAssets = cryptowatchConfig.quoteAssets;
+  const baseAsset = cryptowatchConfig.baseAsset;
+  const exchange = cryptowatchConfig.exchange;
+  const dailyDataNum = cryptowatchConfig.dailyDataNum;
 
-quoteAssets.map(async (quoteAsset: string) => {
-  const ohlcv = new CryptowatchOhlcv(exchange, quoteAsset, baseAsset, initDataNum);
-  const data = await ohlcv.get();
-  ohlcv.insert(data);
+  await Promise.all(
+    quoteAssets.map(async (quoteAsset: string) => {
+      const ohlcv = new CryptowatchOhlcv(exchange, quoteAsset, baseAsset, dailyDataNum);
+      const data = await ohlcv.get();
+      ohlcv.insert(data);
+    })
+  );
+}
+
+cron.schedule("* * * * *", () => {
+  processData;
 });
--- server/batch/init.ts
+++ server/batch/init.ts
@@ -3,14 +3,20 @@ import config from "config";
 import { CryptowatchConfig } from "../../config/config.js";
 import CryptowatchOhlcv from "./lib/cryptowatch/ohlcv.js";
 
-const cryptowatchConfig: CryptowatchConfig = config.get("cryptowatch");
-const quoteAssets = cryptowatchConfig.quoteAssets;
-const baseAsset = cryptowatchConfig.baseAsset;
-const exchange = cryptowatchConfig.exchange;
-const dailyDataNum = cryptowatchConfig.dailyDataNum;
+async function processData() {
+  const cryptowatchConfig: CryptowatchConfig = config.get("cryptowatch");
+  const quoteAssets = cryptowatchConfig.quoteAssets;
+  const baseAsset = cryptowatchConfig.baseAsset;
+  const exchange = cryptowatchConfig.exchange;
+  const initDataNum = cryptowatchConfig.initDataNum;
 
-quoteAssets.map(async (quoteAsset: string) => {
-  const ohlcv = new CryptowatchOhlcv(exchange, quoteAsset, baseAsset, dailyDataNum);
-  const data = await ohlcv.get();
-  ohlcv.insert(data);
-});
+  await Promise.all(
+    quoteAssets.map(async (quoteAsset: string) => {
+      const ohlcv = new CryptowatchOhlcv(exchange, quoteAsset, baseAsset, initDataNum);
+      const data = await ohlcv.get();
+      ohlcv.insert(data);
+    })
+  );
+}
+
+processData();
