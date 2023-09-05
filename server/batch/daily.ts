0aff827918f1f41cec23af89ecb6461ed7dcea3a
MasakiFukunishi <fmsknn@gmail.com>
2023-09-05 12:42:37 -0700

    fix mongo db

--- server/batch/daily.ts
+++ server/batch/daily.ts
@@ -1,23 +1,30 @@
 import config from "config";
 import cron from "node-cron";
 
+import db from "../helpers/mongodb.js";
 import { CryptowatchConfig } from "../types/config.js";
 import CryptowatchOhlcv from "./lib/cryptowatch/ohlcv.js";
 
 async function processData() {
-  const cryptowatchConfig: CryptowatchConfig = config.get("cryptowatch");
-  const quoteAssets = cryptowatchConfig.quoteAssets;
-  const baseAsset = cryptowatchConfig.baseAsset;
-  const exchange = cryptowatchConfig.exchange;
-  const dailyDataNum = cryptowatchConfig.dailyDataNum;
+  db.connect();
 
-  await Promise.all(
-    quoteAssets.map(async (quoteAsset: string) => {
-      const ohlcv = new CryptowatchOhlcv(exchange, quoteAsset, baseAsset, dailyDataNum);
-      const data = await ohlcv.get();
-      ohlcv.insert(data);
-    })
-  );
+  try {
+    const cryptowatchConfig: CryptowatchConfig = config.get("cryptowatch");
+    const quoteAssets = cryptowatchConfig.quoteAssets;
+    const baseAsset = cryptowatchConfig.baseAsset;
+    const exchange = cryptowatchConfig.exchange;
+    const dailyDataNum = cryptowatchConfig.dailyDataNum;
+
+    await Promise.all(
+      quoteAssets.map(async (quoteAsset: string) => {
+        const ohlcv = new CryptowatchOhlcv(exchange, quoteAsset, baseAsset, dailyDataNum);
+        const data = await ohlcv.get();
+        ohlcv.insert(data);
+      })
+    );
+  } finally {
+    db.close();
+  }
 }
 
 cron.schedule("0 0 * * *", () => {
